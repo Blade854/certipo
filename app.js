37e754d6ceaa6038164a368cea58294f504e2709
Blade854 <guedribilal85@gmail.com>
2025-04-01 22:42:28 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+document.getElementById('btn-students').addEventListener('click', () => {
+    alert('فتح واجهة إدارة التلاميذ');
+});
+
+document.getElementById('btn-teachers').addEventListener('click', () => {
+    alert('فتح واجهة إدارة الأساتذة');
+});
+
+document.getElementById('btn-medical').addEventListener('click', () => {
+    alert('فتح واجهة إدارة الشهادات الطبية');
+});
+
+document.getElementById('btn-reports').addEventListener('click', () => {
+    alert('فتح واجهة إدارة التقارير');
+});
+
+document.getElementById('btn-summonses').addEventListener('click', () => {
+    alert('فتح واجهة إدارة الاستدعاءات');
+});
